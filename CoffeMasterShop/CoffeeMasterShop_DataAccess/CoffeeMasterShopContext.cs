41f8ef121a9e58a5bbbf6e30dc7734ec0bafa4c9
poenaru mihai <poenarumihai2008@gmail.com>
2022-10-11 17:17:47 +0300

    Project CoffeMaster Shop

---
+++ CoffeMasterShop/CoffeeMasterShop_DataAccess/CoffeeMasterShopContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeMasterShop_DataAccess
+{
+    public class CoffeeMasterShopContext : DbContext
+    {
+        public CoffeeMasterShopContext(DbContextOptions<CoffeeMasterShopContext> options) : base(options) { }
+    }
+}
